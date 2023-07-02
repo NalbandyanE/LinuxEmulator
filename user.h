cd53e2e8de3aa56fb316b76720026be650912bbb
Emi <89019222+NalbandyanE@users.noreply.github.com>
2023-07-02 15:28:08 +0400

    Update user.h

--- user.h
+++ user.h
@@ -17,9 +17,9 @@ public:
 	int getUid() const;
 	int getGid() const;
 	void useradd(const std::string&);
-    User getUserByUsername(const std::string&) const;
-    void passwd();
-    void id();
+    	User getUserByUsername(const std::string&) const;
+    	void passwd();
+    	void id();
 private:
 	std::string name;
 	std::string password;
@@ -59,48 +59,48 @@ std::string User::getPassword() const {
 }
 
 void User::id() {
-    std::cout << "uid=" << uid << "(" << name << ") gid=" << gid << "(" << name << ")" << std::endl;
+    	std::cout << "uid=" << uid << "(" << name << ") gid=" << gid << "(" << name << ")" << std::endl;
 }
 
 User User::getUserByUsername(const std::string& username) const {
-    for (int i = 0; i < users.size(); ++i) {
-        if (users[i].getName() == username) {
-            return users[i];
-        }
-    }
-    return User();
+   	for (int i = 0; i < users.size(); ++i) {
+        	if (users[i].getName() == username) {
+            		return users[i];
+        	}
+    	}
+    	return User();
 }
 
 void User::useradd(const std::string& username) {
-    User newUser(username, "1111");
-    users.push_back(newUser);
-    std::cout << "User created successfully" << std::endl;
+    	User newUser(username, "1111");
+    	users.push_back(newUser);
+    	std::cout << "User created successfully" << std::endl;
 }
 
 void User::passwd() {
-    std::string currentPassword;
-    std::cout << "Enter current password: ";
-    std::getline(std::cin,currentPassword);
-    if (currentPassword != getPassword()) {
-        std::cout << "Incorrect current password. Password change failed." << std::endl;
-        return;
-    }
-    std::string newPassword;
-    std::string confirmPassword;
-    std::cout << "Enter new password: ";
-    std::getline(std::cin,newPassword);
-    std::cout << "Confirm new password: ";
-    std::getline(std::cin,confirmPassword);
-    if (newPassword.size() < 4) {
-        std::cout << "Invalid new password. Password change failed." << std::endl;
-        return;
-    }
-    if (newPassword != confirmPassword) {
-        std::cout << "New password and confirm password do not match. Password change failed." << std::endl;
-        return;
-    }
-    setPassword(newPassword);
-    std::cout << "Password changed successfully." << std::endl;
+    	std::string currentPassword;
+    	std::cout << "Enter current password: ";
+    	std::getline(std::cin,currentPassword);
+    	if (currentPassword != getPassword()) {
+        	std::cout << "Incorrect current password. Password change failed." << std::endl;
+        	return;
+    	}
+    	std::string newPassword;
+    	std::string confirmPassword;
+    	std::cout << "Enter new password: ";
+    	std::getline(std::cin,newPassword);
+    	std::cout << "Confirm new password: ";
+    	std::getline(std::cin,confirmPassword);
+    	if (newPassword.size() < 4) {
+        	std::cout << "Invalid new password. Password change failed." << std::endl;
+        	return;
+    	}
+    	if (newPassword != confirmPassword) {
+        	std::cout << "New password and confirm password do not match. Password change failed." << std::endl;
+        	return;
+    	}
+    	setPassword(newPassword);
+    	std::cout << "Password changed successfully." << std::endl;
 }
 
 } // namespace LinuxEmulator
